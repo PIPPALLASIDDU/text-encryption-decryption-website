c76bd89c01d1f3e941ad75cc077876581d101eaa
rohan2734 <jagandevaki1@gmail.com>
2021-04-09 20:35:07 +0530

    made the copy to clipoard work

--- Javascriptfiles/CopyFunction.js
+++ Javascriptfiles/CopyFunction.js
@@ -1,14 +1,20 @@
-function cpyFn(){
-    /* Get the text field */
-    var copyText = document.getElementById("output");
-  
-    /* Select the text field */
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); /* For mobile devices */
-  
-    /* Copy the text inside the text field */
-    document.execCommand("copy");
-  
-    /* Alert the copied text */
-    alert("Copied the text: " + copyText.value);
-  }
+function cpyFn() {
+  /* Get the text field */
+  var copyText = document.getElementById("output");
+
+  var textArea = document.createElement("textarea");
+  textArea.value = copyText.textContent;
+  document.body.appendChild(textArea);
+  /* Select the text field */
+  // copyText.select();
+  // copyText.setSelectionRange(0, 99999); /* For mobile devices */
+  textArea.select();
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied the text: " + textArea.value);
+  textArea.remove();
+}
+
+// cpyFn();
